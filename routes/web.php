e7e0a10d585373940aa7ca8905177e2ce23796d0
Sinan Ismaila <sinan@SinanPC.local>
2026-02-06 12:24:33 +0100

    Cleanup diagnostic tools after successful image fix

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\PasswordController;
 
 // Public routes
 Route::get('/', [ProjectController::class, 'showMap'])->name('home');
-Route::get('/debug-images', [\App\Http\Controllers\DebugController::class, 'debugImages']);
 Route::get('/storage/{path}', [\App\Http\Controllers\FileController::class, 'serve'])->where('path', '.*');
 
 // Auth routes
